72c1a52c2a5bc8bbda116b2c07e7c64c24359690
void <111242019+ShockerLL22@users.noreply.github.com>
2025-08-13 22:05:55 +0300

    Update countbot.py

--- countbot.py
+++ countbot.py
@@ -7,7 +7,7 @@ import re
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-MOD_CHANNEL_ID = 1402987082388869282
+CHANNEL_TO_PROMOTE = 1402987082388869282
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -22,18 +22,21 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if message.channel.id == MOD_CHANNEL_ID:
-        if "key" in message.content.lower():
-            await message.reply(f"Hey {message.author.mention}, check out this channel: <#{MOD_CHANNEL_ID}>")
+    content = message.content.lower()
 
-        if link_pattern.search(message.content):
-            try:
-                await message.delete()
-                await message.channel.send(f"Hey {message.author.mention}, don't send links!")
-            except discord.Forbidden:
-                print("Missing permissions to delete messages")
-    
+    if "key" in content:
+        await message.reply(f"Hey {message.author.mention}, check out this channel: <#{CHANNEL_TO_PROMOTE}>")
+
+    if "mango" in content:
+        await message.reply("MENGO MENGO FJWEFHEWHFWHEFEHW")
+
+    if link_pattern.search(message.content):
+        try:
+            await message.delete()
+            await message.channel.send(f"Hey {message.author.mention}, don't send links!")
+        except discord.Forbidden:
+            print("Missing permissions to delete messages")
+
     await bot.process_commands(message)
 
 bot.run(TOKEN)
-
